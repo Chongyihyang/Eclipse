93f89cc40a310d9a9addf5f6c1db5cdcc005f1e4
Chongyihyang <32867847+Chongyihyang@users.noreply.github.com>
2021-12-17 13:42:14 +0800

    Update views.py

--- post/views.py
+++ post/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import *
 from.models import *
 import secrets
-from PIL import IMage as img
+from PIL import Image as img
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
