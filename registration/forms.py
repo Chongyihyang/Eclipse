1ff6473f0cec392baa220334cd9e913021cc8bc1
Chongyihyang <chongyihyang713@gmail.com>
2021-12-21 11:10:07 +0800

    no

--- registration/forms.py
+++ registration/forms.py
@@ -1,8 +1,4 @@
-from django import forms
 from django.contrib.auth.forms import *
-from.models import *
-from django.forms import ModelForm
-from django import forms
 from django.contrib.auth.models import User
 
 class Createuserform(UserCreationForm):
