5f2cc064da456ba8afe7993fe9af7dca67ed495b
Chongyihyang <32867847+Chongyihyang@users.noreply.github.com>
2021-12-21 10:18:28 +0800

    Update views.py

--- registration/views.py
+++ registration/views.py
@@ -12,8 +12,12 @@ def register(request):
             form = Createuserform(request.POST)
             if form.is_valid():
                 form.save()
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=password)
+                login(request, user)
                 messages.success(request, "Account is created")
-                return redirect('/login/')
+                return redirect('/')
         context = {
             'form':form
         }
