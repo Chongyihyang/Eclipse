ca0c76940f40d4429ada6382c05506331716e293
Chongyihyang <32867847+Chongyihyang@users.noreply.github.com>
2021-12-26 22:30:04 +0800

    Update settings.py

--- eclipse/settings.py
+++ eclipse/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '' #NOT TELLING KEK
+SECRET_KEY = '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' #NOT TELLING KEK
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
